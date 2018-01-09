941ace6eea107cd8246f8370e1202d9cda807665
Charles Vazac <cvazac@gmail.com>
2018-01-09 12:48:54 -0600

    change path

--- writers/netlify/index.js
+++ writers/netlify/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const mustache = require('mustache')
+const path = 'build/netlify'
 
 module.exports = function(eventEmitter) {
   const output = {
@@ -10,8 +11,7 @@ module.exports = function(eventEmitter) {
   eventEmitter.on('test', function(header, expectedResults) {
     count++
 
-    const resourceName = `build/server-timing/resources/${count}.js`
-    fs.writeFileSync(resourceName,
+    fs.writeFileSync(`${path}/resources/${count}.js`,
       `testServerTiming(document.currentScript.src, ${JSON.stringify(expectedResults)})\n`)
     output['_headers'] += `/resources/${count}.js\nServer-Timing: ${header}\n\n`
   })
@@ -20,12 +20,12 @@ module.exports = function(eventEmitter) {
       mustache.render(fs.readFileSync(`${__dirname}/index.template.html`).toString(), {testcount: count}))
     writeFile('_headers', output['_headers'])
     fs.createReadStream(`${__dirname}/harness.js`)
-      .pipe(fs.createWriteStream(`./build/server-timing/harness.js`));
+      .pipe(fs.createWriteStream(`./${path}/harness.js`));
   })
 }
 
 function writeFile(fileName, content) {
-  const filePath = `./build/server-timing/${fileName}`
+  const filePath = `./${path}/${fileName}`
   fs.writeFileSync(filePath, content)
   console.info(`${filePath} written.`)
 }
